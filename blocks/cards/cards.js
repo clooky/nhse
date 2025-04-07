01721344c91cd0441f16f7d8faba830fe976d12b
Iain <19688802+clooky@users.noreply.github.com>
2025-04-07 13:49:14 +0100

    Update cards.js
    change

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,10 +3,10 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
-  ul.className = 'nhsuk-grid-row nhsuk-card-group'
+  ul.className = 'nhsuk-grid-row nhsuk-card-group';
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    li.className = 'nhsuk-grid-column-two-thirds nhsuk-card-group__item'
+    li.className = 'nhsuk-grid-column-two-thirds nhsuk-card-group__item';
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
