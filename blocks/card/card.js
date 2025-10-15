b12d697408ad5efab2039b6c5bfcd23e9b80470b
Iain <19688802+clooky@users.noreply.github.com>
2025-10-15 16:04:03 +0100

    Update card.js

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const headerText = block.querySelector('h1').textContent.trim();
-  const bodyHTML = block;
+  const bodyHTML = block.cloneNode(true);
   bodyHTML.querySelector('h1').remove();
   const section = document.createElement('section');
   section.innerHTML = `
