4483ea45b7eced6743531b131c660e9ffbca7140
Iain <19688802+clooky@users.noreply.github.com>
2025-10-15 15:20:19 +0100

    Update card.js

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,5 +1,34 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  /*
+<section>
+    <div class="nhsuk-card nhsuk-card--care nhsuk-card--care--non-urgent">
+        <div class="nhsuk-card--care__heading-container">
+            <h2 class="nhsuk-card--care__heading">
+                <span role="text">
+                    <span class="nhsuk-u-visually-hidden">Non-urgent advice: </span>See a GP if:
+                </span>
+            </h2>
+            <span class="nhsuk-card--care__arrow" aria-hidden="true"></span>
+        </div>
+        <div class="nhsuk-card__content">
+            <ul>
+                <li>you have tummy or back pain that does not go away or keeps coming back</li>
+                <li>you feel a lump in your tummy</li>
+            </ul>
+            <p>These symptoms can be caused by lots of things and do not mean you have an
+                abdominal aortic aneurysm, but it's best to get them checked.</p>
+        </div>
+    </div>
+</section>
+*/
+const section = document.createElement('section');
+const cardWrapper = document.createElement('div');
+cardWrapper.className = 'nhsuk-card nhsuk-card--care nhsuk-card--care--non-urgent';
+section.append(cardWrapper);
 
+  
+block.textContent = '';
+block.append(section);
 }
