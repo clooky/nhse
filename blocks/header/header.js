9d8bbeb9a18a2622e9f1c0181991bdba6a02d9d3
Iain <19688802+clooky@users.noreply.github.com>
2025-10-15 17:39:14 +0100

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -169,6 +169,31 @@ export default async function decorate(block) {
 
 export default async function decorate(block) {
   const headerHTML =`
+ <header class="nhsuk-header" role="banner" data-module="nhsuk-header">
+  <div class="nhsuk-header__container nhsuk-width-container">
+    <div class="nhsuk-header__service">
+      <a class="nhsuk-header__service-logo" href="#">
+        <svg class="nhsuk-header__logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 80" height="40" width="100" focusable="false" role="img" aria-label="NHS digital service manual homepage">
+          <title>NHS digital service manual homepage</title>
+          <path fill="currentcolor" d="M200 0v80H0V0h200Zm-27.5 5.5c-14.5 0-29 5-29 22 0 10.2 7.7 13.5 14.7 16.3l.7.3c5.4 2 10.1 3.9 10.1 8.4 0 6.5-8.5 7.5-14 7.5s-12.5-1.5-16-3.5L135 70c5.5 2 13.5 3.5 20 3.5 15.5 0 32-4.5 32-22.5 0-19.5-25.5-16.5-25.5-25.5 0-5.5 5.5-6.5 12.5-6.5a35 35 0 0 1 14.5 3l4-13.5c-4.5-2-12-3-20-3Zm-131 2h-22l-14 65H22l9-45h.5l13.5 45h21.5l14-65H64l-9 45h-.5l-13-45Zm63 0h-18l-13 65h17l6-28H117l-5.5 28H129l13.5-65H125L119.5 32h-20l5-24.5Z" />
+        </svg>
+        <span class="nhsuk-header__service-name">Digital service manual</span>
+      </a>
+    </div>
+    <search class="nhsuk-header__search">
+      <form class="nhsuk-header__search-form" id="search" action="https://www.nhs.uk/search/" method="get">
+        <label class="nhsuk-u-visually-hidden" for="search-field">Search the NHS digital service manual</label>
+        <input class="nhsuk-header__search-input nhsuk-input" id="search-field" name="q" type="search" placeholder="Search" autocomplete="off">
+        <button class="nhsuk-header__search-submit" type="submit">
+          <svg class="nhsuk-icon nhsuk-icon--search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" focusable="false" role="img" aria-label="Search">
+            <title>Search</title>
+            <path d="m20.7 19.3-4.1-4.1a7 7 0 1 0-1.4 1.4l4 4.1a1 1 0 0 0 1.5 0c.4-.4.4-1 0-1.4ZM6 11a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" />
+          </svg>
+
+        </button>
+      </form>
+    </search>
+  </div>
   <nav class="nhsuk-header__navigation" aria-label="Menu">
     <div class="nhsuk-header__navigation-container nhsuk-width-container">
       <ul class="nhsuk-header__navigation-list">
