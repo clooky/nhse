10cd6746953e8025db786c9a849707d4b7572606
Iain <19688802+clooky@users.noreply.github.com>
2025-10-15 15:31:55 +0100

    Update card.js

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,12 +1,14 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
+  const headerText = "See a GP if:";
+  
   const cardBody = `
       <div class="nhsuk-card nhsuk-card--care nhsuk-card--care--non-urgent">
         <div class="nhsuk-card--care__heading-container">
             <h2 class="nhsuk-card--care__heading">
                 <span role="text">
-                    <span class="nhsuk-u-visually-hidden">Non-urgent advice: </span>See a GP if:
+                    <span class="nhsuk-u-visually-hidden">Non-urgent advice: </span>${headerText}
                 </span>
             </h2>
             <span class="nhsuk-card--care__arrow" aria-hidden="true"></span>
@@ -21,31 +23,7 @@ export default function decorate(block) {
                 abdominal aortic aneurysm, but it's best to get them checked.</p>
         </div>
     </div>
-
   `
-  /*
-<section>
-    <div class="nhsuk-card nhsuk-card--care nhsuk-card--care--non-urgent">
-        <div class="nhsuk-card--care__heading-container">
-            <h2 class="nhsuk-card--care__heading">
-                <span role="text">
-                    <span class="nhsuk-u-visually-hidden">Non-urgent advice: </span>See a GP if:
-                </span>
-            </h2>
-            <span class="nhsuk-card--care__arrow" aria-hidden="true"></span>
-        </div>
-        
-        <div class="nhsuk-card__content">
-            <ul>
-                <li>you have tummy or back pain that does not go away or keeps coming back</li>
-                <li>you feel a lump in your tummy</li>
-            </ul>
-            <p>These symptoms can be caused by lots of things and do not mean you have an
-                abdominal aortic aneurysm, but it's best to get them checked.</p>
-        </div>
-    </div>
-</section>
-*/
 const section = document.createElement('section');
 section.insertAdjacentHTML('beforeend', cardBody);
   
