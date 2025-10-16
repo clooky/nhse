8bb25b57e98edafee5c8fd1e8076200f297fccd4
Iain <19688802+clooky@users.noreply.github.com>
2025-10-16 08:57:21 +0100

    Add wrapper div around main element

--- scripts/decorators/mainElement.js
+++ scripts/decorators/mainElement.js
@@ -5,9 +5,15 @@
  */
 export function decorateMainElement(mainEl) {
   if (!mainEl) return;
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('nhsuk-width-container');
+  
   // Example: static or dynamic attribute values:
   mainEl.setAttribute('data-region', window.location.hostname.includes('localhost') ? 'dev' : 'prod');
   mainEl.setAttribute('data-site-version', '1.0.0');  // or fetch from meta / config
   // More attributes:
   mainEl.classList.add('nhsuk-main-wrapper');
+
+  mainEl.parentNode.insertBefore(wrapper,mainEl);
+  wrapper.appendChild(mainEl);
 }
