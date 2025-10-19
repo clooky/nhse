a69a9350ab7083087e84917f105278d1c6adbdc1
Iain <19688802+clooky@users.noreply.github.com>
2025-10-19 12:56:34 +0100

    Create backlink.js

---
+++ blocks/backlink/backlink.js
@@ -0,0 +1,11 @@
+const backLinkTemplate = `<a class="nhsuk-back-link" href="[HREF]">[LINKTEXT]</a>`;
+
+export default function decorate(block) {
+  const cardAnchor = currentCard.querySelector('a');
+  const linkHref = cardAnchor?.getAttribute('href') || '';
+  const linkText = cardAnchor?.textContent || '';
+  block.textContent = '';
+  block.innerHTML = backLinkTemplate;
+  block.querySelector('a').href = href;
+  block.querySelector('a').innerHTML = linkText;
+}
