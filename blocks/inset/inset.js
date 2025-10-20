3abd9f047cf8369ad4f0b4397905d47319616dd2
Iain <19688802+clooky@users.noreply.github.com>
2025-10-20 13:44:25 +0100

    Update inset.js

--- blocks/inset/inset.js
+++ blocks/inset/inset.js
@@ -2,10 +2,10 @@ export default async function decorate(block) {
   const insetType = block.children[0].innerText;
   const visualImparedInfoSpan = document.createElement('span');
   const visualImparedInfoText = document.createTextNode(insetType);
-  visualImparedInfoSpan.className= 'nhsuk-u-visually-hidden';
+  visualImparedInfoSpan.className = 'nhsuk-u-visually-hidden';
   visualImparedInfoSpan.appendChild(visualImparedInfoText);
-  
-  block.children[1].className= 'nhsuk-inset-text';
+
+  block.children[1].className = 'nhsuk-inset-text';
   block.children[1].insertBefore(visualImparedInfoSpan, block.children[1].children[0]);
   block.children[0].remove();
 }
