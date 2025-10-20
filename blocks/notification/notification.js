1ae0d8d2c56c8e496f6f7d47145a2f6fb81b0a79
Iain Clucas <clooky@gmail.com>
2025-10-20 23:52:40 +0100

    Add success to notification

--- blocks/notification/notification.js
+++ blocks/notification/notification.js
@@ -1,5 +1,5 @@
-const makeBanner = (title, html) => `
-<div class="nhsuk-notification-banner" role="region" aria-labelledby="nhsuk-notification-banner-title" data-module="nhsuk-notification-banner">
+const makeBanner = (title, bannerType, html) => `
+<div class="nhsuk-notification-banner ${bannerType}" role="region" aria-labelledby="nhsuk-notification-banner-title" data-module="nhsuk-notification-banner">
   <div class="nhsuk-notification-banner__header">
     <h2 class="nhsuk-notification-banner__title" id="nhsuk-notification-banner-title">
       ${title}
@@ -14,8 +14,8 @@ const makeBanner = (title, html) => `
 export default async function decorate(block) {
   const title = (block.children.length > 0) ? block.children[0].innerText : '';
   const html = (block.children.length > 1) ? block.children[1].innerHTML : '';
-
-  const bannerBlock = makeBanner(title, html);
+  const bannerType = block.classList.contains('success') ? 'nhsuk-notification-banner--success' : '';
+  const bannerBlock = makeBanner(title, bannerType, html);
   block.textContent = '';
   block.innerHTML = bannerBlock;
 }
